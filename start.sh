d6a8fd1192f142159f46895b62d105e104ac51c9
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 00:41:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/SohanRazz/Shortnerv2sss.git /Shortnerv2sss
-cd /Shortnerv2sss
+git clone https://github.com/SohanRazz/Newshortneroggy.git /Newshortneroggy
+cd /Newshortneroggy
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
